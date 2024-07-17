a4f34868b8f9b489010c29e93b59e20f33cec71a
Sreelekha Mallojwala <sreelekha.mallojwala@solugenix.com>
2024-07-17 22:35:25 +0530

    Initial commit to project

---
+++ src/main/java/sgx/testing/automation/layout_testing_poc/LayoutTestingPocApplication.java
@@ -0,0 +1,13 @@
+package sgx.testing.automation.layout_testing_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutTestingPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayoutTestingPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sgx/testing/automation/layout_testing_poc/LayoutTestingPocApplicationTests.java
@@ -0,0 +1,13 @@
+package sgx.testing.automation.layout_testing_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutTestingPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
